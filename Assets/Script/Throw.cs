e13040de2a49165e05f921f94b409fc49a0fdc5b
Brocoliii <puree.suwa@bumail.net>
2025-03-26 16:36:47 +0700

    //Script
    Made it shoot properly, now it can shoot.

--- Assets/Script/Throw.cs
+++ Assets/Script/Throw.cs
@@ -4,15 +4,16 @@ using UnityEngine.InputSystem;
 public class Throw : MonoBehaviour
 {
     public float speed;
-    public float cdTime;
     public float mass;
     public float force = 0f;
-    
+    public float newForce = 0f;
+    public int chargeCount = 0;
+    public Transform shootPoint;
+
     public float chargeSpeed = 1f;
     //private bool onClick = false;
-
-
     private bool isChargingUp = true;
+    public GameObject ammoPrefab;
 
 
 
@@ -22,56 +23,68 @@ public class Throw : MonoBehaviour
     }
     void Update()
     {
+       
         Charging();
+        
     }
     void Charging()
 
     {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            chargeCount++;
+        }
         if (Input.GetKey(KeyCode.Mouse1))
         {
-           
-            if (Input.GetKey(KeyCode.Mouse1))  
-                if (isChargingUp)  
+            if (isChargingUp)
+            {
+                force += chargeSpeed * Time.deltaTime;
+
+                if (force >= 100f)
                 {
-                    force += chargeSpeed * Time.deltaTime;  
-
-                    
-                    if (force >= 100f)
-                    {
-                        force = 100f;
-                        isChargingUp = false; 
-                    }
+                    force = 100f;
+                    isChargingUp = false;
                 }
-                else  
-                {
-                    force -= chargeSpeed * Time.deltaTime;  
-
-                    
-                    if (force <= 0f)
-                    {
-                        force = 0f;
-                        isChargingUp = true;  
-                    }
-                }
-
-               
-                Debug.Log("Force: " + force);
             }
             else
             {
-                
-                Debug.Log("Force (เมื่อปล่อยปุ่ม): " + force);
+                force -= chargeSpeed * Time.deltaTime;
 
-
-
-
-
-
-
-            }
-            void shoot()
-            {
-                Debug.Log(force);
+                if (force <= 0f)
+                {
+                    force = 0f;
+                    isChargingUp = true;
+                }
             }
         }
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            if (chargeCount > 0)
+            {
+                newForce = force;
+                shoot();
+            
+            }
+            force = 0f;
+            isChargingUp = true;
+                Debug.Log("newforce" + newForce);
+
+            }
+
+        }
+    void shoot()
+    {
+        if (chargeCount > 0)
+        {
+            GameObject ammo = Instantiate(ammoPrefab, shootPoint.position, transform.rotation);
+            Rigidbody ammoRb = ammo.GetComponent<Rigidbody>();
+            if (ammoRb != null)
+            {
+
+                ammoRb.AddForce(shootPoint.forward * newForce, ForceMode.Impulse);
+            }
+           chargeCount--;
+        }
+    }
 }
+
